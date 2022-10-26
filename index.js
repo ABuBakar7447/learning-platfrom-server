f296d58f8c4d198d59237c39dbf23d5bf95e5c5f
ABuBakar7447 <lone7447wolf@gmail.com>
2022-10-26 21:25:36 +0600

    couse json data now visible in online server

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const coursedata = require('./data/coursedata.json');
+
+
 app.get('/', (req, res) =>{
     res.send('Api Running')
 });
 
+app.get('/course-data', (req, res) =>{
+    res.send(coursedata)
+})
 app.listen(port, () =>{
     console.log('Server runing on port', port)
 })
